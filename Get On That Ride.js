0c76ecbfd782696c6227cc663a805b89a2602334
farouking <71613134+farouking@users.noreply.github.com>
2024-01-24 11:37:41 +0100

    Add files via upload

---
+++ Get On That Ride.js
@@ -0,0 +1,14 @@
+
+var Age = 12
+var Height = 45
+
+function getThatRid(Age,Height){
+    var minimumAge = 10;
+    var minimumHeight = 42;
+if (Height<minimumHeight){console.log("Sorry kiddo.May be next year ")}
+else 
+    if (Age< minimumAge) {console.log("Sorry kiddo.May be next year ")}
+    else console.log("Get onthat ride,Kiddo!")
+
+} 
+getThatRid(Age,Height)
