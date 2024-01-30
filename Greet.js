d9b3ef68baa82784806dff6cb3bcbb326a72f8c7
farouking <71613134+farouking@users.noreply.github.com>
2024-01-30 13:08:42 +0100

    Add files via upload

---
+++ Greet.js
@@ -0,0 +1,21 @@
+//Level 01
+function GreetL1(name){
+    console.log("Good Day "+name + " !");
+}
+GreetL1("Mohamed");
+
+//Level 02
+function Greet(name,time){
+    console.log("Good "+time+ " "+name + " !");
+}
+Greet("Farouk","Evening");
+
+//Level 03
+function Greet(name,time){
+    if (name=="Count Dooku"){
+        console.log("I'm coming for you Dooku !")
+    }
+    else {console.log("Good "+time+ " "+name + " !");}
+    }
+Greet("Ali","night");
+Greet("Count Dooku","night");
